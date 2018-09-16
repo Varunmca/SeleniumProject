e798332bb0b29b14d5e16bf43d31c5c7e9a67df8
varunmca <varunkumarpce@gmail.com>
2018-09-16 11:37:04 +0530

    conflicts

--- src/test/java/org/vwdautomation/test/homepage/A.java
+++ src/test/java/org/vwdautomation/test/homepage/A.java
@@ -1,5 +1,13 @@
 package org.vwdautomation.test.homepage;
 
 public class A {
+	
+	public static void main (String args[])
+	{
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+	}
 
 }
